36b282e5d0a521d35ffa47226870bf319b06417d
serebrs <serebr.s@gmail.com>
2022-03-26 12:45:58 +0600

    Hint сделал выпадающим сверху

--- src/components/utils/HintBox.vue
+++ src/components/utils/HintBox.vue
@@ -4,7 +4,7 @@
     <transition name="fade">
       <div
         v-show="isActive"
-        class="absolute inset-x-0 top-full z-10 w-fit py-1 px-2 text-[0.75rem] text-center whitespace-normal leading-3 text-white bg-gray-700 shadow-md rounded-md"
+        class="absolute inset-x-0 bottom-[120%] z-10 w-fit py-1 px-2 text-[0.75rem] text-center whitespace-normal leading-3 text-white bg-gray-600 shadow-md rounded-md"
       >{{ hintText }}</div>
     </transition>
   </div>
@@ -27,10 +27,12 @@ export default {
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.3s ease;
+  transition: transform 0.3s ease;
 }
 
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+  transform: translateY(5px);
 }
 </style>
