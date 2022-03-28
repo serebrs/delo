2493d69acaca09a187f8734c8d467c893852a863
serebrs <serebr.s@gmail.com>
2022-03-28 12:44:30 +0600

    Стилизовал + закрытие по клику снаружи

--- src/components/utils/ModalBox.vue
+++ src/components/utils/ModalBox.vue
@@ -1,19 +1,50 @@
 <template>
-  <div
-    class="absolute z-40 inset-y-0 inset-x-0 flex justify-center items-center space-x-0 space-y-0 h-screen bg-slate-700 bg-opacity-50 overflow-hidden backdrop-blur-sm"
-  >
+  <transition name="fade">
     <div
-      class="flex flex-col justify-center items-center w-96 rounded-md shadow-md bg-white border border-gray-50"
+      @click.self="$emit('modalClose')"
+      class="absolute z-40 inset-y-0 inset-x-0 flex justify-center items-center space-x-0 space-y-0 h-screen bg-slate-700 bg-opacity-50 overflow-hidden backdrop-blur-sm"
     >
-      <button @click="$emit('modalClose')" class="p-2 bg-slate-400 rounded-md">Закрыть</button>
+      <div
+        class="flex flex-col justify-center items-center w-96 rounded-md shadow-md bg-white border border-gray-100"
+      >
+        <button
+          @click="$emit('modalClose')"
+          class="p-1 m-1 text-gray-600 bg-gray-100 hover:bg-gray-200 rounded-md justify-self-start self-end"
+        >
+          <XIcon class="h-5 w-5" />
+        </button>
+        <div class="p-5">
+          <p>Контент</p>
+          <p>Контент</p>
+          <p>Контент</p>
+          <p>Контент</p>
+          <p>Контент</p>
+          <button class="p-2 w-32 text-gray-700 bg-gray-100 hover:bg-gray-200 shadow-sm rounded-md mt-4">Да</button>
+        </div>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
+import { XIcon } from "@heroicons/vue/solid"
+
 export default {
   emits: [
-    "modalClose"
-  ]
+    'modalClose'
+  ],
+  components: { XIcon }
 }
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
